ae3e01b94db7799d4798f830a40eecea3c9b37b4
chablo <charles.blosseville@gmail.com>
2018-06-28 16:55:39 +0200

    Testing some things

--- AtlantisJavaEE-ejb/target/generated-sources/annotations/com/atlantis/domain/User_.java
+++ AtlantisJavaEE-ejb/target/generated-sources/annotations/com/atlantis/domain/User_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-28T15:10:35")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-28T16:53:30")
 @StaticMetamodel(User.class)
 public class User_ { 
 
