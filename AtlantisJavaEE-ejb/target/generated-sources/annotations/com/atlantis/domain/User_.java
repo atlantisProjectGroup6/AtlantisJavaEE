0f7745973bf31b8462afda550d77de972d554748
mireen <m.ibarboure@hotmail.fr>
2018-06-29 01:12:44 -0700

    I'm here

--- AtlantisJavaEE-ejb/target/generated-sources/annotations/com/atlantis/domain/User_.java
+++ AtlantisJavaEE-ejb/target/generated-sources/annotations/com/atlantis/domain/User_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-29T09:23:34")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-29T01:09:57")
 @StaticMetamodel(User.class)
 public class User_ { 
 
